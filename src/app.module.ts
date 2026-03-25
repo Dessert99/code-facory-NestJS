768dc9925f9006af7603b0514f6552aa56d1632a
Dessert99 <lsjh1234@naver.com>
2026-03-25 10:57:21 +0900

    2026/03/25

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModel } from 'src/posts/entities/posts.entitiy';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
+// postgres 세팅
 @Module({
-  imports: [PostsModule], // 다른 모듈을 불러올 때 imports 사용
+  imports: [
+    PostsModule, // 다른 모듈을 불러올 때 imports 사용
+
+    // nest - postgres 연결하기
+    TypeOrmModule.forRoot({
+      type: 'postgres', // 데이터베이스 타입
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [PostModel], //생성할 모델, DB와 연동될 모델을 넣기
+      synchronize: true, // DB, TypeORM 싱크를 맞추기 (개발환경에서는 true, 프로덕션 환경에서는 DB구조가 바뀌기 떄문에 false)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
